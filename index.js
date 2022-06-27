efe892030368e83a48c053c510569b2e45217259
Mark Nguyen <mgb.nguyen@gmail.com>
2022-06-27 23:00:55 +0100

    Lighting setup

--- index.js
+++ index.js
@@ -3,47 +3,106 @@ import {
     OrbitControls
 } from 'https://unpkg.com/three@0.139.2/examples/jsm/controls/OrbitControls.js';
 
-export function main() {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+import {
+    GLTFLoader
+} from 'https://unpkg.com/three@0.139.2/examples/jsm/loaders/GLTFLoader.js';
 
+let model, sun;
+
+function createRenderer() {
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.toneMapping = THREE.ReinhardToneMapping;
+    renderer.toneMappingExposure = 3;
+    renderer.shadowMap.enabled = true;
+    return renderer;
+}
 
+function createCamera() {
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
+    return camera;
+}
+
+function addModel(scene) {
+    const loader = new GLTFLoader();
+    loader.load('assets/little_hermit_crab/scene.gltf', result => {
+        model = result.scene.children[0];
+        model.traverse(obj => {
+            if (obj.isMesh) {
+                obj.castShadow = true;
+                obj.receiveShadow = true;
+
+                if (obj.material.map) {
+                    obj.material.map.anisotrophy = 16;
+                }
+            }
+        });
+        scene.add(model);
+
+
+        // Ground plane
+        const geo = new THREE.PlaneGeometry(6, 6, 8, 8);
+        const mat = new THREE.MeshPhongMaterial({ color: 0xffffff, side: THREE.DoubleSide });
+        const plane = new THREE.Mesh(geo, mat);
+        plane.rotateX(- Math.PI / 2);
+        plane.position.y -= 0.5;
+        plane.receiveShadow = true;
+        scene.add(plane);
+    });
+}
+
+function addCubemap(scene) {
+    const path = "https://threejs.org/examples/textures/cube/skyboxsun25deg/";
+    const format = '.jpg';
+    const urls = [
+        `${path}px.jpg`,
+        `${path}nx.jpg`,
+        `${path}py.jpg`,
+        `${path}ny.jpg`,
+        `${path}pz.jpg`,
+        `${path}nz.jpg`,
+    ];
+    const loader = new THREE.CubeTextureLoader();
+    const cubeMap = loader.load(urls);
+    cubeMap.format = THREE.RGBFormat;
+
+    scene.background = cubeMap;
+}
+function addLights(scene) {
+    const skyCol = new THREE.Color(0.513, 0.835, 0.866);
+    const groundCol = new THREE.Color(0.815, 0.184, 0.439);
+    const hemiLight = new THREE.HemisphereLight(skyCol, groundCol, 2);
+    scene.add(hemiLight);
+
+    sun = new THREE.SpotLight(0xffffff, 2.2);
+    sun.castShadow = true;
+    sun.shadow.bias = -0.0001;
+    sun.shadow.mapSize = new THREE.Vector2(4096, 4096);
+    scene.add(sun);
+}
+
+export function main() {
+    const scene = new THREE.Scene();
+    const camera = createCamera();
+    const renderer = createRenderer();
     const controls = new OrbitControls(camera, renderer.domElement);
-
     document.body.appendChild(renderer.domElement);
 
-    // Material
-    const material = new THREE.MeshPhongMaterial({
-        color: new THREE.Color(1, 1, 1)
-    });
-
-    // Cube
-    const boxGeo = new THREE.BoxGeometry();
-    const boxMesh = new THREE.Mesh(boxGeo, material);
-    scene.add(boxMesh);
-
-    // Light
-    const fillLight = new THREE.DirectionalLight(new THREE.Color(0.08, 0.86, 0.9), 3);
-    fillLight.position.set(-1, 2, 4);
-    scene.add(fillLight);
-
-    const rimLight = new THREE.DirectionalLight(new THREE.Color(0.827, 0.466, 0.854), 1);
-    rimLight.position.set(4, 0.4, -1);
-    scene.add(rimLight);
-
-
-    camera.position.z = 5;
+    addCubemap(scene);
+    addLights(scene);
+    addModel(scene);
 
     function render(time) {
         time *= 0.001; // convert time to seconds
 
-        boxMesh.rotation.x = time;
-        boxMesh.rotation.y = time;
+        sun.position.set(
+            camera.position.x + 5,
+            camera.position.y + 20,
+            camera.position.z + 10,
+        );
 
         renderer.render(scene, camera);
-
         requestAnimationFrame(render);
     }
     requestAnimationFrame(render);
